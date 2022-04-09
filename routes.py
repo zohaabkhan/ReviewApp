2b903b3be6ae5a211bfeccbfba06daa8d7ef1670
Monica <mbyrd3@student.gsu.edu>
2022-04-08 21:56:42 -0400

    lecture videos

--- routes.py
+++ routes.py
@@ -1,3 +1,15 @@
+#pylint: disable=invalid-name
+#pylint: disable=missing-final-newline
+#pylint: disable=line-too-long
+#pylint: disable=unpacking-non-sequence
+#pylint: disable=superfluous-parens
+#pylint: disable=trailing-whitespace
+#pylint: disable=wrong-import-order
+#pylint: disable=unused-import
+#pylint: disable=invalid-envvar-default
+
+
+
 from app import app, db
 import random
 import os
@@ -15,16 +27,25 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_name):
+    """
+    function to handle user login 
+    """    
     return User.query.get(user_name)
 
 
 @app.route("/signup")
 def signup():
+    """
+    function to handle user sign up
+    """
     return flask.render_template("signup.html")
 
 
 @app.route("/signup", methods=["POST"])
 def signup_post():
+    """
+    doc string: user sign up routing
+    """
     username = flask.request.form.get("username")
     user = User.query.filter_by(username=username).first()
     if user:
@@ -39,11 +60,17 @@ def signup_post():
 
 @app.route("/login")
 def login():
+    """
+    doc string: user login
+    """
     return flask.render_template("login.html")
 
 
 @app.route("/login", methods=["POST"])
 def login_post():
+    """
+    doc string: login navigation
+    """
     username = flask.request.form.get("username")
     user = User.query.filter_by(username=username).first()
     if user:
@@ -56,13 +83,27 @@ def login_post():
 
 @app.route("/")
 def landing():
+    """
+    doc string: landing page navigation
+    """
     if current_user.is_authenticated:
         return flask.redirect("index")
     return flask.redirect("login")
 
 
+@app.route("/videos") 
+def gallery():
+    """
+    doc string: navigate to video gallery
+    """   
+    return flask.render_template("videos.html")
+
+
 @app.route("/logout")
 def logout():
+    """
+    doc string: user logout naviagtion
+    """
     logout_user()
     return flask.redirect("login")
 
@@ -70,9 +111,12 @@ def logout():
 @app.route("/index")
 @login_required
 def index():
+    """
+    doc string: render index page
+    """
     return flask.render_template(
         "index.html",
-    )
+    )    
 
 
 if __name__ == "__main__":
